79c4655155deabecd643adbfacde7090ea7b6768
Kuba Sobczyński <jak.sobczynski@gmail.com>
2022-05-26 18:27:02 +0200

    update

--- src/index.js
+++ src/index.js
@@ -6,17 +6,32 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
+const multer = require('multer')
 
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is currently down. Check back soon!')
-// })
+const upload = multer({
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb){
+        if(!file.originalname.match(/\.(doc|docx)$/)){
+            return cb(new Error('Please upload a PDF'))
+        }
+
+        cb(undefined, true)
+        // cb(new Error('File must a PDF'))
+        // cb(undefinded, true)
+        // cb(undefinded, false)
+
+
+    }
+})
+
+app.post('/upload', upload.single('upload'), (req, res)=>{
+    res.send()
+},(error, req, res, next)=>{
+    res.status(400).send({error: error.message})
+})
 
 app.use(express.json())
 app.use(userRouter)
@@ -25,19 +40,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async ()=>{
-    // const task = await Task.findById('628e4ec1fdd501067091f0d6')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-
-    const user = await User.findById('628e4deaa757722d0c6032fb')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
-
-}
-
-main()
--- src/routers/task.js
+++ src/routers/task.js
@@ -22,14 +22,36 @@ router.post('/tasks', auth, async (req, res)=>{
   
 })
 
+// GET /tasks?completed=false
+// GET /tasks?limit=10&skip=0
+// GET /tasks?sortBy=createdAt_desc
 router.get('/tasks', auth, async (req, res)=>{
 
+     const match = {}
+     const sort = {}
+
+     if(req.query.completed){
+        match.completed = req.query.completed === 'true'
+     }
+
+     if(req.query.sortBy){
+         const parts = req.query.sortBy.split(':')
+         sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+     }
+
     try{
-       //const tasks = await Task.find({ owner: req.user._id}) 
-       await req.user.populate('tasks').execPopulate()
+       await req.user.populate({
+           path: 'tasks',
+           match,
+           options: {
+               limit: parseInt(req.query.limit),
+               skip: parseInt(req.query.skip),
+               sort
+           }
+       }).execPopulate()
        res.status(200).send(req.user.tasks)
     }catch(e){
-        res.status(400).send()
+        res.status(500).send()
     }
     
 })
@@ -75,11 +97,11 @@ router.patch('/tasks/:id', auth, async (req, res)=>{
 
  router.delete('/tasks/:id', auth, async (req, res)=>{
     try{
-       const task = await Task.findByOneAndDelete({_id: req.params.id, owner: req.user._id})
-       await  task.save()
+       const task = await Task.findOneAndDelete({_id: req.params.id, owner: req.user._id})
+      
 
        if(!task){
-           return res.status(404).send()
+           res.status(404).send()
        }
 
        res.send(task)
